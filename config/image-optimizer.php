ba972d4c5869a2a60f288770ef9176bfebf2b662
David Torralbo <davidtorralboperez@gmail.com>
2022-07-27 23:12:05 +0200

    Add optimizer component options

---
+++ config/image-optimizer.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    /*
+     * Image Optimizer Component Options
+     */
+    'component' => [
+        'formats' => ['png', 'jpeg', 'webp', 'avif'],
+        'default_format' => 'jpeg',
+    ],
+];
