b5ffc3c5ad26737aece24f4d36a882341f82edd7
David Torralbo <davidtorralboperez@gmail.com>
2022-11-25 18:06:51 +0100

    Expect width and height at image optimizer class

--- src/ImageOptimizer.php
+++ src/ImageOptimizer.php
@@ -6,8 +6,8 @@ use Torralbodavid\ImageOptimizer\Services\Squoosh\Compress;
 
 class ImageOptimizer
 {
-    public function image(string $image): Compress
+    public function image(string $image, ?string $width = null, ?string $height = null): Compress
     {
-        return new Compress($image);
+        return new Compress($image, $width, $height);
     }
 }
--- src/ImageOptimizerServiceProvider.php
+++ src/ImageOptimizerServiceProvider.php
@@ -11,7 +11,7 @@ class ImageOptimizerServiceProvider extends PackageServiceProvider
     {
         $package
             ->name('image-optimizer')
-            ->hasConfigFile('image-optimizer')
+            ->hasConfigFile()
             ->hasViews();
     }
 }
