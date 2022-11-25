afdd017bb52f21dc67a11e0f6bc6c4a61e083cae
David Torralbo <davidtorralboperez@gmail.com>
2022-11-25 18:05:57 +0100

    Add output directory at config file

--- config/image-optimizer.php
+++ config/image-optimizer.php
@@ -8,6 +8,7 @@ return [
         'formats' => ['png', 'jpeg', 'webp', 'avif'],
         'default_format' => 'jpeg',
     ],
+    'output_directory' => 'cache/',
 
     'npx' => [
         'command' => 'export PATH=\$PATH:~/bin; npx @squoosh/cli',
